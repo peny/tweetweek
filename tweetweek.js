0f9df3b3d7e7892b3660a5924d269dda7ac06809
Petter Nyman <petter@pttr.se>
2013-09-11 14:03:00 +0200

    using just one file isn't really that cool, split the app to two

--- tweetweek.js
+++ tweetweek.js
@@ -1,10 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 var _ = require('underscore');
 
 
 /*
  * Returns an array for every hour in the week
- * each array entry contains the number of tweets 
+ * each array entry contains the number of tweets
  * for screen_name that week and the total number
  * of retweets of those tweets
  */
@@ -48,12 +49,13 @@ function getTimeline(screen_name, callback) {
 }
 
 /*
- * Get the 200 last tweets from screen_name and 
+ * Get the 200 last tweets from screen_name and
  * returns them as JSON in the callback response
  */
 function getTwitterData(screen_name, callback){
   http.get('http://api.twitter.com/1/statuses/user_timeline.json?count=200&trim_user=true&screen_name=' + screen_name, function(res) {
     var err = null;
+    console.log(err,res)html;
     if(res.statusCode !== 200){
       err = res.statusCode;
     }
@@ -70,46 +72,14 @@ http.createServer(function(req,res){
   console.log(req.url);
     if(req.url === '/'){
       res.writeHead(200, {'Content-Type': 'text/html'});
-      //More files === less fun
-      var html = "<!DOCTYPE html><html>"+
-        "<head><title>Tweet Week</title>"+
-        "<link href='//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap.no-responsive.no-icons.min.css' rel='stylesheet'>"+
-        "<link href='http://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>"+
-      "<script language='javascript' type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>"+
-        "<script language='javascript' type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/flot/0.7/jquery.flot.min.js'></script>"+
-        "<script language='javascript' type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js '></script>"+
-        "</head>"+
-        "<body style='font-family: &quot;Montserrat&quot;, sant-serif;'>"+
-        "<div class='container offset-1'>"+
-        "<h1>Tweet Week</h1>"+
-        "<p>Enter your twitter handle to get to know when you should post your next tweet to get as many retweets as possible</p>"+
-        "<div class='row offset1'>"+
-        "<div id='searchbuttons' class='input-append'>"+
-        "<input id='search' type='text' class='span3' placeholder='Twitter handle'>"+
-        "<button class='btn' onclick='renderGraph()'>Search!</button>"+
-        "</div></div>"+
-        "<div id='graph' style='width:1100px; height: 400px;'></div>"+
-        "<div id='daygraph' style='width:1100px; height: 400px;'></div>"+
-        "A <a href='http://pttr.se'>pttr</a> production</p>"+
-        "<script>function renderGraph(){$.getJSON('/user/'+$('#search').val()+'.json',function(){}).success(function(data){"+
-        "var total = _.map(data,function(hours){return [hours.hour,hours.total_tweets];});"+
-        "var rts = _.map(data,function(hours){return [hours.hour,hours.retweets];});"+
-        "$.plot($('#graph'),[{label: 'tweets', data: total},{label: 'retweets', data: rts}],{series:{ lines: {show: true}, points: {show: true}}, xaxis: { ticks: [[0, 'Sunday'], ,[12, '12:00'], [24, 'Monday'],[36,'12:00'],[48, 'Tuesday'],[60, '12:00'],[72,'Wednesday'],[84,'12:00'],[96, 'Thursday'],[108,'12:00'],[120,'Friday'],[132,'12:00'],[144,'Saturday'],[156,'12:00']]}});"+
-        "var daytotal = _.map(data,function(hours){return [hours.hour%24,hours.total_tweets];});"+
-        "var grouped = _.groupBy(daytotal,function(hour){ return hour[0];});"+
-        "daytotal = _.map(_.keys(grouped),function(hour){var current = grouped[hour]; return [current[0][0],_.reduce(current,function(memo,num){ return memo+num[1];},0)];});"+
-        "var dayrts = _.map(data,function(hours){return [hours.hour%24,hours.retweets];});"+
-        "grouped = _.groupBy(dayrts,function(hour){ return hour[0];});"+
-        "dayrts = _.map(_.keys(grouped),function(hour){var current = grouped[hour]; return [current[0][0],_.reduce(current,function(memo,num){ return memo+num[1];},0)];});"+
-        "$.plot($('#daygraph'),[{label: 'tweets', data: daytotal},{label: 'retweets', data: dayrts}],{series:{ lines: {show: true}, points: {show: true}}, xaxis: { ticks: [[0, '0:00'], ,[6, '6:00'], [12, '12:00'],[18,'18:00'],[23, '23:00']]}});"+
-        "}).error(function(data){if(data.status === 500){$('#graph').prepend(data.responseText);}})}</script>"+
-        "</div></body></html>";
-      res.end(html);
+      fs.readFile('index.html', function(err, data){
+        res.end(data);
+      });
     } else if((/^\/user\/\w{1,30}\.json$/i).test(req.url)){
       var user = req.url.match(/\w+/gi);
       user = user[1];
       getTimeline(user,function(err,data){
-        if(err){  
+        if(err){
           res.writeHead(500, {'Content-Type': 'text/plain'});
           res.end("<h2>You killed it!</h2><h3>The user probably doesn't exist</h3><iframe width='560' height='315' src='http://www.youtube.com/embed/CqyEUAoKcz0?start=354&autoplay=1' frameborder='0' allowfullscreen></iframe>");
         } else {
